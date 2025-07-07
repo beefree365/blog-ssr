b6819863450924fc1d3f58ce60d55fab18623abf
beefree365 <beefree365@163.com>
2025-07-07 23:04:54 +0800

    up

--- blog.config.js
+++ blog.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  nickname: "程小白",
-  desc: "编程手艺人",
+  nickname: "远方",
+  desc: "奔向远方的人",
   avatar: "/avatar.png", // 头像图片放在 public 目录下
   stats: {
     posts: 21,
@@ -9,8 +9,7 @@ module.exports = {
   },
   github: "https://github.com/yourname",
   about: `
-    <p>你好，我是程小白，一名热爱编程的手艺人。</p>
-    <p>喜欢前端、云原生、自动化和极简设计。</p>
+    <p>你好，我是远方，一名奔向远方的人。</p>
     <p>欢迎通过 <a href="https://github.com/yourname" target="_blank">GitHub</a> 联系我！</p>
   `
 }; 
