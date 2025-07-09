439c49326b852d71b5d3a157061a0c9037dca092
beefree365 <beefree365@163.com>
2025-07-09 18:38:43 +0800

    up

--- blog.config.js
+++ blog.config.js
@@ -9,7 +9,8 @@ module.exports = {
   },
   github: "https://github.com/yourname",
   about: `
-    <p>你好，我是远方，一名奔向远方的人。</p>
-    <p>欢迎通过 <a href="https://github.com/yourname" target="_blank">GitHub</a> 联系我！</p>
+    <p>你好，我是远方，一名奔向自由的人。</p>
+    <p>欢迎通过 <a href="https://github.com/beefree365" target="_blank">GitHub</a> 联系我！</p>
+    <p>我的主页博客：<a href="https://beefree365.github.io/" target="_blank">https://beefree365.github.io/</a></p>
   `
 }; 
